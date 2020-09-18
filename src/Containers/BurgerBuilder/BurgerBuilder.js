5d025d1b2f71093f5196de0ea3cfafaae6cbd2f8
Ross <rossbaker32@gmail.com>
2020-09-18 17:00:17 +1000

    Add controls and buttons for modifying the burger ingredients

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import BuildControls from '../../Components/Burger/BuildControls/BuildControls';
 
 import Burger from '../../Components/Burger/Burger';
 class BurgerBuilder extends Component {
@@ -16,7 +17,7 @@ class BurgerBuilder extends Component {
     return (
       <>
         <Burger ingredients={this.state.ingredients}/>
-        <div>Build Controls</div>
+        <BuildControls />
       </>
     );
   }
