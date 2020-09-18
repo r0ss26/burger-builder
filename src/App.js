a6b596941e394aa27a0906ee34a7b0f1c240a452
Ross <rossbaker32@gmail.com>
2020-09-18 15:07:18 +1000

    Add layour component with toolbar and burger builder component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Layout from './Components/Layout/Layout';
+
+import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
     <div>
-      
+      <Layout>
+        <BurgerBuilder></BurgerBuilder>
+      </Layout>
     </div>
   );
 }
---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import classes from './Layout.module.css';
+
+const Layout = (props) => {
+  return (
+    <>
+      <div>Toolbar, SideDrawer, Backdrop</div>
+      <main className={classes.Content}>
+        {props.children}
+      </main>
+    </>
+  )
+}
+
+export default Layout;
