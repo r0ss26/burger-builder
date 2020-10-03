1d60988bd7bb4672cd026575299dd1763d49b592
Ross <rossbaker32@gmail.com>
2020-10-03 17:56:25 +1000

    Convert app component to arrow function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Layout from './hoc/Layout/Layout';
 
 import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder';
 
-function App() {
+const App = () => {
   return (
     <Layout>
       <BurgerBuilder></BurgerBuilder>
